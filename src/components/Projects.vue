ccc20a5947382de9f9b1d299e90a33894972b0d4
Mouhamed Lawal Dan Azoumi <82711872+law-san-667@users.noreply.github.com>
2023-04-10 00:59:05 +0000

    try fix images problem

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -53,9 +53,99 @@ export default {
     
     data() {
         return {
-            projects: [],
-            displayedProject: {},
-            images: [],
+            projects: [
+                {
+                    id: 0,
+                    name: "SchoolUp",
+                    description: `Another colaborative project. This is a school management app that I made with Java SE and JavaFX. It's a desktop app that allows you to manage students, classes, courses, etc...\n
+                    I worked on the backend and my two associates worked on the frontend and files managements`,
+                    images: [
+                        "../assets/schoolup1-e88827bd.png",
+                        "../assets/schoolup2-3adf6ec1.png",
+                        "../assets/schoolup3-91c8eb67.png",
+                    ],
+                    link: "https://github.com/azimari-toure-ikbal/projet-java-gestion-scolarite"
+                },
+
+                {
+                    id: 1,
+                    name: "flutter-law-gpt",
+                    description: "This is a mobile app that uses OpenAi API to implement chat functionality with different models that we can choose.",
+                    images: [
+                        "../assets/law_gpt-3ea59182.png",
+                    ],
+                    link: "https://github.com/law-san-667/flutter-law-gpt"
+                },
+                {
+                    id: 2,
+                    name: "Lawmine shop",
+                    description: "An App with laravel to manage products (yeah basically it's like a todolist but with products).<br>This is a collaborative project, I worked on the backend with laravel and my friend worked on the frontend with Tailwind.",
+                    images: [
+                        "../assets/lawmine-42e05ef6.png",
+                        "../assets/lawmine2-8eb3b854.png",
+                        "../assets/lawmine3-bf8e8057.png"
+                    ],
+                    link: "https://github.com/law-san-667/gestion-produits-laravel"
+                },
+                {
+                    id: 3,
+                    name: "Plant App",
+                    description: "This is an app of presentation of some plant in order to buy them. There is no back so actually you can't really buy anything haha.<br>That for me a good way to learn Flutter basics.",
+                    images: [
+                        "../assets/plant_app1-e85b2bcc.png",
+                        "../assets/plant_app2-13cbc9fd.png"
+                    ],
+                    link: "https://github.com/law-san-667/flutter-plant-app"
+                },
+                {
+                    id: 4,
+                    name: "Booking Law",
+                    description: "This is a flight booking app with admin, users & companies interfaces made with PHP.<br>Really cool.",
+                    images: [
+                        "../assets/booking-66f7e186.png",
+                        "../assets/booking2-24791369.png",
+                        "../assets/booking3-672a915e.png"
+                    ],
+                    link: "https://github.com/law-san-667/php-booking-flight-app"
+                },
+                {
+                    id: 5,
+                    name: "Vue task tracker",
+                    description: "This is a basic task tracker made with VueJS and tailwind. Json server was used for the \"back-end\".",
+                    images: [
+                        "../assets/vue_todo-ab33f516.png",
+                        "../assets/vue_todo2-ca2716a8.png"
+                    ],
+                    link: "https://github.com/law-san-667/vue-task-tracker"
+                },
+                {
+                    id: 6,
+                    name: "Angular todo-list",
+                    description: "This is a todo-list app made with Angular & Bootstrap. It's a simple app that allows you once you are logged in to add, delete, update and mark as done your tasks.<br>I used JSON server to simulate a backend.",
+                    images: [
+                        "../assets/ng_todo1-0f10ae40.png",
+                        "../assets/ng_todo2-13a924b4.png",
+                    ],
+                    link: "https://github.com/law-san-667/angular-todolist-app"
+                },
+
+            ],
+            displayedProject: {
+                id: 0,
+                name: "SchoolUp",
+                description: "Another colaborative project. This is a school management app that I made with Java SE and JavaFX. It's a desktop app that allows you to manage students, classes, courses, etc...<br>I worked on the backend and my two associates worked on the frontend and files managements",
+                images: [
+                        "../assets/schoolup1-e88827bd.png",
+                        "../assets/schoolup2-3adf6ec1.png",
+                        "../assets/schoolup3-91c8eb67.png",
+                    ],
+                link: "https://github.com/azimari-toure-ikbal/projet-java-gestion-scolarite"
+            },
+            images: [
+                        "../assets/schoolup1-e88827bd.png",
+                        "../assets/schoolup2-3adf6ec1.png",
+                        "../assets/schoolup3-91c8eb67.png",
+                    ],
             index: 0
         }
     },
